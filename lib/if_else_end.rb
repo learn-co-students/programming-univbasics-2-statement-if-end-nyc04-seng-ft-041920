24b74b16ff79fc9170c10071abccdcc4789f940a
Ian McKenzie <imckenzie777@gmail.com>
2020-04-24 06:36:51 +0000

    Done.

--- lib/if_else_end.rb
+++ lib/if_else_end.rb
@@ -1,1 +1,43 @@
-# Write your solution here
+if true
+  puts "This code runs!"
+end
+if false
+  puts "This code does not run. You will not see this in IRB"
+end
+run_code_inside = true
+puts "Code before if...end"
+if run_code_inside
+  puts "code inside"
+end
+puts "Code after if...end"
+
+chance_of_rain = 1#0.2
+puts "Let's go outside!"
+if chance_of_rain > 2
+  puts "Pack an umbrella!"
+else
+  puts "Enjoy the fine day!"
+end
+puts "Oh, and always wear sunscreen!"
+
+chance_of_rain = 0.2
+if chance_of_rain <= 0.25
+  puts "Pack a sun shelter!"
+elsif (chance_of_rain > 0.25 && chance_of_rain < 0.75)
+  puts "Pack an umbrella!"
+else
+  puts "Stay home and read Hegel."
+end
+
+puts "You know what year it is??"
+this_year = 2020
+puts "Hey, it's 2020!" if this_year == 2020
+
+current_time = Time.now
+current_time = current_time.to_i
+
+if current_time % 2 == 0
+  puts "Even!"
+else
+  puts "Odd!"
+end
